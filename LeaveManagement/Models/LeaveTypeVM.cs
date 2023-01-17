0427322826bee364a10964ae693065c2310319aa
payam <payamzohari80@gmail.com>
2023-01-17 13:30:01 +0330

    Add CRUD operations for LeaveType module

---
+++ LeaveManagement/Models/LeaveTypeVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Models
+{
+    public class LeaveTypeVM 
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name="Default Number of Days")]
+        [Required]
+        [Range(2, 20, ErrorMessage = "please inter a valid number")]
+        public int DefaultDays { get; set; }
+    }
+}
